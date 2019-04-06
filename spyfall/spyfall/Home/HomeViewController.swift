06bc998966f3f434bb15bfdc8231c16a7e85a5c8
Josiah.Rininger <josiahrininger@gmail.com>
2019-04-06 00:21:24 -0500

    Created Project

---
+++ spyfall/spyfall/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  spyfall
+//
+//  Created by Josiah Rininger on 4/6/19.
+//  Copyright © 2019 Josiah Rininger. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
