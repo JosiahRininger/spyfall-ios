e68ac945d84092b037a44fe731d57c514cac7c6d
Josiah.Rininger <josiahrininger@gmail.com>
2019-04-08 08:51:45 -0500

    Finishes Home Screen

--- spyfall/spyfall/Home/HomeViewController.swift
+++ spyfall/spyfall/Home/HomeViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var joinGameButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        newGameButton.layer.borderColor = #colorLiteral(red: 0.4392156863, green: 0.4392156863, blue: 0.4392156863, alpha: 1)
+        newGameButton.layer.borderWidth = 1
+        joinGameButton.layer.borderColor = #colorLiteral(red: 0.4392156863, green: 0.4392156863, blue: 0.4392156863, alpha: 1)
+        joinGameButton.layer.borderWidth = 1
+        
     }
     
 
---
+++ spyfall/spyfall/New Game/NewGameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewGameViewController.swift
+//  spyfall
+//
+//  Created by Josiah Rininger on 4/8/19.
+//  Copyright © 2019 Josiah Rininger. All rights reserved.
+//
+
+import UIKit
+
+class NewGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
